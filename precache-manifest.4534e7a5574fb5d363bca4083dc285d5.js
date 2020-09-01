48a8923f4829b9e5c84a9d6fda07095d507fe265
tomerk97 <tomerk1997@gmail.com>
2020-09-01 20:57:24 +0300

    Updates

---
+++ precache-manifest.4534e7a5574fb5d363bca4083dc285d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f756d1bcfeada5c5d147ca49ce5e45",
+    "url": "/tomerk97/OnlineShmonline/index.html"
+  },
+  {
+    "revision": "ebd324d2c9f95a420cce",
+    "url": "/tomerk97/OnlineShmonline/static/css/main.8a83f234.chunk.css"
+  },
+  {
+    "revision": "e482d41c6f1ef7b50f03",
+    "url": "/tomerk97/OnlineShmonline/static/js/2.a6a3aec4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tomerk97/OnlineShmonline/static/js/2.a6a3aec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd324d2c9f95a420cce",
+    "url": "/tomerk97/OnlineShmonline/static/js/main.6b91476c.chunk.js"
+  },
+  {
+    "revision": "3c2b280bdff8b353f4c0",
+    "url": "/tomerk97/OnlineShmonline/static/js/runtime-main.cea13721.js"
+  },
+  {
+    "revision": "00ef7ea74404b45ce343c7d0b0968bfd",
+    "url": "/tomerk97/OnlineShmonline/static/media/SecularOne-Regular.00ef7ea7.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tomerk97/OnlineShmonline/static/media/logo.5d5d9eef.svg"
+  }
+]);
