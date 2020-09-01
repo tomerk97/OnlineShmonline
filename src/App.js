63b48c53c33f3a7c65e8a8f4997bd06131efe959
tomerk97 <tomerk1997@gmail.com>
2020-09-01 22:02:34 +0300

    one

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
-import {HashRouter as Router,HashRouter,Route,Switch,Link,Redirect} from 'react-router-dom';
+import {BrowserRouter as Router,HashRouter,Route,Switch,Link,Redirect} from 'react-router-dom';
 import MainPage from "./pages/index";
 import dashboard from "./pages/dashboard";
 import forgot from "./pages/forgot";
@@ -13,16 +13,25 @@ import status from "./pages/status";
 class App extends Component {
 
   render() {
-      return( 
+      const App = () => (
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/list' component={List}/>
+        </Switch>
+      </div>
+    )
 
-       <Router basename='/'>
+
+      return( 
+       <Switch basename='/'>
         <Route exact path="/" component ={MainPage}/> 
         <Route path="/dashboard" component ={dashboard}/> 
         <Route path="/forgot" component ={forgot}/> 
         <Route path="/sign" component ={sign}/> 
         <Route path="/payments" component ={payments}/> 
         <Route path="/status" component ={status}/> 
-       </Router>
+       </Switch>
 
       );
   }
